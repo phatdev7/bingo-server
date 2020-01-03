7133311679444634dea54abc05b284b89baca424
Phat Nguyen <phat.nguyen@s3corp.com.vn>
2020-01-03 17:48:06 +0700

    npm install socket.io

--- src/routes/api/card.ts
+++ src/routes/api/card.ts
@@ -2,10 +2,10 @@ import { Router } from 'express';
 const route = Router();
 import { generate } from 'src/actions/card';
 
-route.post('/', async (req, res) => {
+route.get('/', async (req, res) => {
   generate(6, 4)
-    .then(data => {
-      res.json({ data });
+    .then(card => {
+      res.json({ card });
     })
     .catch(err => res.status(404).json({ errors: err }));
 });
---
+++ src/services/socket.ts
@@ -0,0 +1,8 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const io = require('socket.io')();
+
+class Socket {
+  constructor() {}
+}
+
+export default new Socket();
